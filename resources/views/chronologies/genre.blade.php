df83fcddb50254497048b3775c8bf28ef4efb218
Wataru-Nishimura <nw.job.hunting@gmail.com>
2022-12-05 21:19:29 +0900

    スマホ対応2

--- resources/views/chronologies/genre.blade.php
+++ resources/views/chronologies/genre.blade.php
@@ -1,14 +1,41 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
+    <head prefix="og: https://ogp.me/ns#">
         <meta charset="UTF-8">
-        <title>ジャンル別年表 | 歴史年表 Only View Chronology</title>
+        <title>ジャンル別年表 | 歴史年表 Only View Chronology 見るだけで歴史がわかる・面白くなる</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--SEO施策-->
+        <meta name="google-site-verification" content="Ksr6Vy1GFSnzPiDKlkWHE1Rdjr-3I5Ki45I7RSux0MI" />
+        <meta name="description" content="毎日が歴史上の出来事の記念日。身近な人の誕生日と同じ日にあった出来事を知ることで、歴史との距離を縮めます。">
+        <meta name="keywords" content="歴史年表, 日本史, 今日は何の日？, 寺社仏閣, 社会" />
+        <meta property="og:title" content="ジャンルごとの歴史年表が見られます" />
+		<meta property="og:description" content="毎日が歴史上の出来事の記念日。身近な人の誕生日と同じ日にあった出来事を知ることで、歴史との距離を縮めます。" />
+		<meta property="og:type" content="article" />
+		<meta property="og:url" content="https://only-view-chronology.herokuapp.com/genre" />
+		<meta name="twitter:card" content="summary" />
+		<meta name="twitter:site" content="@only_view_chron" />
+		<meta property="fb:app_id" content="526978166155996" />
+		<!--<meta property="og:image" content="サムネイル画像の URL" />-->
+		<meta property="og:site_name" content="歴史年表 | Only View Chronology" />
+		<meta property="og:locale" content="ja_JP"  />
+        <!--SEO施策-->
         <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP&display=swap" rel="stylesheet">
+        <!--機能編 6-1-2 フェードイン・アウトさせて全画面で見せる-->
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
         <!--CSS-->
         <link rel="stylesheet" type="text/css" href="{{ secure_asset('/css/parts.css') }}">
         <link rel="stylesheet" type="text/css" href="{{ secure_asset('/css/style.css') }}">
         <link rel="stylesheet" type="text/css" href="{{ secure_asset('/css/hamburgermenu.css') }}">
         <!--CSS-->
+        <!--favicon-->
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+		<link rel="manifest" href="/site.webmanifest">
+		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#26304c">
+		<meta name="msapplication-TileColor" content="#2b5797">
+		<meta name="theme-color" content="#ffffff">
+        <!--favicon-->
     </head>
     <body>
         <div id="container">
